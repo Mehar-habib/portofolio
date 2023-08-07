15e68e375a226b709426cae939ddd43071f7c19a
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-08-07 22:54:43 +0500

    working the themes changer section

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -1,6 +1,6 @@
 import { themes } from "../data";
 import { FaCog } from "react-icons/fa";
-import { BsMoon } from "react-icons/bs";
+import { BsMoon, BsSun } from "react-icons/bs";
 import "./themes.css";
 import ThemeItem from "./ThemeItem";
 import { useEffect, useState } from "react";
@@ -13,17 +13,40 @@ const getStorageColor = () => {
   return color;
 };
 
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 function Themes() {
   const [showSwitcher, setShowSwitcher] = useState(false);
   const [color, setColor] = useState(getStorageColor());
+  const [theme, setTheme] = useState(getStorageTheme());
 
   const changeColor = (color) => {
     setColor(color);
   };
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
   useEffect(() => {
     document.documentElement.style.setProperty("--first-color", color);
     localStorage.setItem("color", color);
   }, [color]);
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return (
     <div>
@@ -35,8 +58,8 @@ function Themes() {
           <FaCog />
         </div>
 
-        <div className="theme__toggler">
-          <BsMoon />
+        <div className="theme__toggler" onClick={toggleTheme}>
+          {theme === "light-theme" ? <BsMoon /> : <BsSun />}
         </div>
 
         <h3 className="style__switcher-title">Style Switcher</h3>
