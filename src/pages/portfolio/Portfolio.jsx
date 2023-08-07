3d61f8e4a068be0e6ff90b4a3e0693f626808d7b
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-08-07 18:41:12 +0500

    working in portfolio section

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,5 +1,23 @@
+import PortfolioItem from "../../components/PortfolioItem";
+import { portfolio } from "../../data";
+import "./portfolio.css";
+
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <>
+      <div className="portfolio section">
+        <h2 className="section__title">
+          {" "}
+          My <span>Portfolio</span>{" "}
+        </h2>
+        <div className="portfolio__container container grid">
+          {portfolio.map((item) => {
+            return <PortfolioItem key={item.id} {...item} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Portfolio;
