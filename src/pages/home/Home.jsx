e43bfecb89886b132e6ec327e55ebbdb670d2de1
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-08-07 13:19:38 +0500

    working on home page and style in button

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,37 @@
+import Profile from "../../assets/home.jpg";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import "./home.css";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="home section grid">
+        <img src={Profile} alt="" className="home__img" />
+
+        <div className="home__content">
+          <div className="home__data">
+            <h1 className="home__title">
+              <span>I&apos;m Mehar Habib.</span> Web Developer
+            </h1>
+
+            <p className="home__description">
+              I&apos;m a Pakistan based web designer & Full Stack developer
+              focused on crafting clean & user-friendly experiences, I am
+              passionate about building excellent software that improves the
+              lives of those around me.
+            </p>
+            <Link to="/about" className="button">
+              More About Me{" "}
+              <span className="button__icon">
+                <FaArrowRight />
+              </span>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Home;
