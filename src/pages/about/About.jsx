97c5f6bacc982bdd7945fe987b23c64ea9097f25
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2023-08-07 17:29:45 +0500

    complete About section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,6 +4,8 @@ import CV from "../../assets/cv.pdf";
 import Stats from "../../components/Stats";
 import "./about.css";
 import Skills from "../../components/Skills";
+import { resume } from "../../data";
+import ResumeItem from "../../components/ResumeItem";
 
 function About() {
   return (
@@ -42,6 +44,32 @@ function About() {
           <h3 className="section__subtitle subtitle__center">My Skills</h3>
           <div className="skills__container grid">
             <Skills />
+          </div>
+        </section>
+
+        {/* seParator */}
+        <div className="separator"></div>
+        {/* Education */}
+        <section className="resume">
+          <h3 className="section__subtitle subtitle__center">
+            Experience & Education
+          </h3>
+          <div className="resume__container grid">
+            <div className="resume__data">
+              {resume.map((val) => {
+                if (val.category === "experience") {
+                  return <ResumeItem key={val.id} {...val} />;
+                }
+              })}
+            </div>
+
+            <div className="resume__data">
+              {resume.map((val) => {
+                if (val.category === "education") {
+                  return <ResumeItem key={val.id} {...val} />;
+                }
+              })}
+            </div>
           </div>
         </section>
       </main>
